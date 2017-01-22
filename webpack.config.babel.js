60370973dd511970e8745002839a9f490fe557ae
Matt Travi <programmer@travi.org>
2017-01-21 21:20:29 -0600

    feat(example): got the example presentation working

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,39 +1,82 @@
-/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+/* eslint import/no-extraneous-dependencies: ['error', {'devDependencies': true}] */
 import webpack from 'webpack';
 import {getIfUtils, removeEmpty} from 'webpack-config-utils';
+import AssetsPlugin from 'assets-webpack-plugin';
+import Visualizer from 'webpack-visualizer-plugin';
+import LodashModuleReplacementPlugin from 'lodash-webpack-plugin';
+import CleanPlugin from 'clean-webpack-plugin';
 
 export default function (env) {
-  const {ifDev} = getIfUtils(env);
+  const {ifDevelopment, ifProduction} = getIfUtils(env);
+  const assetsPath = './lib';
 
   return {
-    devtool: ifDev('eval-source-map', 'source-map'),
+    // devtool: ifDevelopment('eval-source-map', 'source-map'),
+    devtool: false,
     entry: {
-      main: './src/index',
-      vendor: ['react', 'react-dom']
+      example: './src/index',
+      vendor: ['react', 'react-dom', 'spectacle']
     },
     output: {
-      path: './lib',
-      filename: '[name]-[hash].js'
+      path: assetsPath,
+      filename: '[name].js'
     },
     module: {
-      rules: [{
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: [['es2015', {modules: false}], 'react']
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          query: {
+            presets: [['es2015', {modules: false}], 'react'],
+            plugins: removeEmpty([
+              ifProduction('transform-react-remove-prop-types'),
+              ifDevelopment('transform-react-jsx-source'),
+              'transform-runtime',
+              'lodash'
+            ]),
+            comments: false,
+            cacheDirectory: true
+          }
+        }, {
+          test: /\.css$/,
+          loader: 'style-loader!css-loader'
+        },
+        {
+          test: /\.(png|jpg|gif)$/,
+          loader: 'url-loader?limit=8192'
+        },
+        {
+          test: /\.svg$/,
+          loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
         }
-      }]
+      ]
     },
     plugins: removeEmpty([
+      ifProduction(new CleanPlugin([assetsPath], {root: __dirname})),
+      new AssetsPlugin(),
       new webpack.DefinePlugin({
         'process.env': {
-          NODE_ENV: JSON.stringify(env)
+          // NODE_ENV: JSON.stringify(env)
+          NODE_ENV: JSON.stringify('production')
         }
       }),
+      ifDevelopment(new webpack.NamedModulesPlugin()),
       new webpack.optimize.CommonsChunkPlugin({
         names: ['vendor', 'manifest']
-      })
+      }),
+      // ifProduction(new webpack.LoaderOptionsPlugin({
+      //   minimize: true,
+      //   debug: false
+      // })),
+      // ifProduction(new webpack.optimize.UglifyJsPlugin({
+      //   compress: {
+      //     screw_ie8: true,
+      //     warnings: false
+      //   }
+      // })),
+      ifProduction(new Visualizer()),
+      ifProduction(new LodashModuleReplacementPlugin())
     ])
   };
 }
