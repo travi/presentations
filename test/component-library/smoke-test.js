624ac82e6e54dc3f0e17c53614d7a5a340aef30d
Matt Travi <programmer@travi.org>
2017-04-23 14:19:17 -0500

    test(smoke): added a smoke test for the component-library presentation

---
+++ test/component-library/smoke-test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {mount} from 'enzyme';
+import Presentation from '../../src/component-library/presentation';
+
+suite('smoke tests for the component-library presentation', () => {
+  test('that the presentation renders', () => {
+    mount(<Presentation />);
+  });
+});
