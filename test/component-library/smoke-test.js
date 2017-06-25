b2351615f6409d3374a3f94dd650d77d79744c2f
Matt Travi <programmer@travi.org>
2017-06-25 00:16:57 -0500

    test(pinned): added smoke tests for the pinned versions of the talks

--- test/component-library/smoke-test.js
+++ test/component-library/smoke-test.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import {mount} from 'enzyme';
 import Presentation from '../../src/component-library/presentation';
+import PresentationDsmJs from '../../src/component-library-dsmjs-july-2016/presentation';
 
 suite('smoke tests for the component-library presentation', () => {
   test('that the presentation renders', () => {
     const wrapper = mount(<Presentation />);
+
+    wrapper.unmount();
+  });
+
+  test('that the presentation given at dsmjs renders', () => {
+    const wrapper = mount(<PresentationDsmJs />);
 
     wrapper.unmount();
   });
--- test/continuous-deployment/smoke-test.js
+++ test/continuous-deployment/smoke-test.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import {mount} from 'enzyme';
-import Example from '../../src/continuous-deployment/presentation';
+import ContinuousDeployment from '../../src/continuous-deployment/presentation';
+import ContinuousDeploymentDsmJs from '../../src/continuous-deployment-dsmjs-june-2017/presentation';
 
 suite('smoke tests for the continuous-deployment presentation', () => {
   test('that the presentation renders', () => {
-    const wrapper = mount(<Example />);
+    const wrapper = mount(<ContinuousDeployment />);
+
+    wrapper.unmount();
+  });
+
+  test('that the presentation given at dsmjsrenders', () => {
+    const wrapper = mount(<ContinuousDeploymentDsmJs />);
 
     wrapper.unmount();
   });
